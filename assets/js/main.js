cb0bdc48e63ec23b895c1434dd31a06be1cd5a3f
Alessandro <internicola.alessandro@gmail.com>
2021-01-19 23:09:13 +0100

    Header completato
    Aggiunto il carousel dinamico tramite VueJs, da rivedere alcuni margini e correggere la dimensione della card del jumbotron, aggiungere i dettagli delle mini immagini

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,11 +37,26 @@ let root = new Vue({
       }
     ],
 
+    jumboText: [
+      {
+        title: "Devotion that never ends",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, unde, enim cupiditate amet ipsum id"
+      },
+      {
+        title: "Projects made with love",
+        subTitle: "Cose scritte a caso che servono a non usare sempre lo stesso lorem ipsum che è molto utile ma anche basta"
+      },
+      {
+        title: "Our new folio full of joy",
+        subTitle: "lref edfuuuer ioeujf eoijoijojo fee fefefsef oij aojgvòawe aòeoifj e aòoie"
+      }
+    ],
     carouselImages:[
       "./assets/img/h-2-slider-img-11.png",
       "./assets/img/h-2-slider-img-15.png",
       "./assets/img/short-slider-rev-1-img-3.png",
-    ]
+    ],
+    counter: 0
   },
 
   mounted() {
@@ -49,7 +64,23 @@ let root = new Vue({
   },
 
   methods: {
+    next(){
+      this.counter++;
+      if(this.counter > this.carouselImages.length-1){
+        this.counter = 0;
+      }
+    },
 
+    prev(){
+      this.counter--;
+      if(this.counter < 0){
+        this.counter = this.carouselImages.length-1;
+      }
+    },
+  },
+
+  created(){
+    setInterval(this.next, 5000);
   },
 
 });
