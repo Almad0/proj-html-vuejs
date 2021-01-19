e9b8be1846826b6b2af2a67890dd69f2b56987b0
Alessandro <internicola.alessandro@gmail.com>
2021-01-19 19:26:00 +0100

    Navbar completata
    Sistemati colori, margini, posizionamenti tramite flex della navbar, personalizzato il cursore e l'hover tramite l'after-selector, importati i link direttamente da VueJs tramite v-for, impostati come oggetti per utilizzare i sottomenù in futuro

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,45 @@ let root = new Vue({
   el: "#root",
 
   data: {
-    lingua: "",
+    logo: "./assets/img/logo-sidearea-1.png",
+    navLink: [
+      {
+        name: "home",
+        url: "www.google.com",
+        subMenu: [{}],
+      },
+      {
+        name: "pages",
+        url: "www.google.com",
+        subMenu: [{}],
+      },
+      {
+        name: "portfolio",
+        url: "www.google.com",
+        subMenu: [{}],
+      },
+      {
+        name: "blog",
+        url: "www.google.com",
+        subMenu: [{}],
+      },
+      {
+        name: "shop",
+        url: "www.google.com",
+        subMenu: [{}],
+      },
+      {
+        name: "elements",
+        url: "www.google.com",
+        subMenu: [{}],
+      }
+    ],
+
+    carouselImages:[
+      "./assets/img/h-2-slider-img-11.png",
+      "./assets/img/h-2-slider-img-15.png",
+      "./assets/img/short-slider-rev-1-img-3.png",
+    ]
   },
 
   mounted() {
@@ -11,7 +49,7 @@ let root = new Vue({
   },
 
   methods: {
-    
+
   },
 
 });
